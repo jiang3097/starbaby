7b0a0b6087ce9ae63c9f1853567918dbe0966cae
user5338173185 <3986208642574327-user5338173185@noreply.coze.cn>
2026-05-05 15:19:26 +0800

    fix: 恢复UserProvider，修复星小宝形象同步问题

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
+import { UserProvider } from './context/UserContext'
 import { AppProvider } from './context/AppContext'
 import TimeLimitModal from './components/TimeLimitModal'
 import './index.css'
@@ -10,10 +11,12 @@ import './framework/theme.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-      <AppProvider>
-        <App />
-        <TimeLimitModal />
-      </AppProvider>
+      <UserProvider>
+        <AppProvider>
+          <App />
+          <TimeLimitModal />
+        </AppProvider>
+      </UserProvider>
     </HashRouter>
   </React.StrictMode>,
 )
