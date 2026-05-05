0da5070bf81e88a0a587a64b56e9a8bac3561201
user5338173185 <3986208642574327-user5338173185@noreply.coze.cn>
2026-05-05 20:17:24 +0800

    fix: 更新星小宝形象图片URL

--- src/lib/starAvatars.ts
+++ src/lib/starAvatars.ts
@@ -5,28 +5,28 @@ export const STAR_AVATARS = [
     name: '星星',
     color: 'from-amber-200 to-yellow-300',
     emoji: '⭐',
-    image: 'https://pic.imgdb.cn/item/64f1a2d066821a3c85c0e3c0.png',
+    image: 'https://code.coze.cn/api/sandbox/coze_coding/file/proxy?expire_time=-1&file_path=assets%2F%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE+2026-05-05+112106_20260505201550867.png&nonce=7ee57fe3-3bf2-4971-927e-0600e555ea45&project_id=7635954527711035402&sign=91f473fbff7db65fcbbf0f4dd5d9d9785f0322c29722c379d6ed1fe9c7338b43',
   },
   {
     id: 2,
     name: '月亮',
     color: 'from-slate-200 to-slate-300',
     emoji: '🌙',
-    image: 'https://pic.imgdb.cn/item/64f1a2d066821a3c85c0e3c1.png',
+    image: 'https://code.coze.cn/api/sandbox/coze_coding/file/proxy?expire_time=-1&file_path=assets%2F%E5%B1%8F%E5%B9%95%E6%88%AA%E5%9B%BE+2026-05-05+112122_20260505201545562.png&nonce=494c3199-bc1a-4e26-9763-ec612dedf721&project_id=7635954527711035402&sign=e63cfccb0f7bdce2cb36d1c5c435efe36ea8b1c74766e57475f0f5825316a158',
   },
   {
     id: 3,
     name: '太阳',
     color: 'from-orange-300 to-amber-400',
     emoji: '☀️',
-    image: 'https://pic.imgdb.cn/item/64f1a2d066821a3c85c0e3c2.png',
+    image: 'https://code.coze.cn/api/sandbox/coze_coding/file/proxy?expire_time=-1&file_path=assets%2F%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20260505112032_161_75_20260505201613321.jpg&nonce=2a5ad64d-c4bb-43f6-bede-6a660d4eb4a4&project_id=7635954527711035402&sign=9b469101da9ee583ab88b116b5942cd83b46d97b758e7dc21aa4092b89d74546',
   },
   {
     id: 4,
     name: '云朵',
     color: 'from-sky-200 to-blue-300',
     emoji: '☁️',
-    image: 'https://pic.imgdb.cn/item/64f1a2d066821a3c85c0e3c3.png',
+    image: 'https://code.coze.cn/api/sandbox/coze_coding/file/proxy?expire_time=-1&file_path=assets%2F%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20260505112035_163_75_20260505201620273.jpg&nonce=c026622d-23e4-43bb-a511-15b38b676a5e&project_id=7635954527711035402&sign=14ad00c211d10265a261d9533f1acd89145707f6cac1369aa65d2aeb2e640eb4',
   },
 ];
 
